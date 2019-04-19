311c97fbdbe0b9d3bca7b8450bee6cb984826712
Chenyang-Zhang <49511259+Chenyang-Zhang@users.noreply.github.com>
2019-04-19 20:54:38 +0800

    Update git_homework.c

--- git_homework.c
+++ git_homework.c
@@ -3,6 +3,7 @@
 int main()
 {
         printf("Git is very interesting!");
-	  printf("Git is easy to use!");
+	printf("Git is easy to use!");
+	printf("I like git very much!");
         return 0;
 }
