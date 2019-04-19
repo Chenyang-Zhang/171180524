877ed3688b52b56e63db2e0e3496f73bc413942a
Chenyang Zhang <695017814@qq.com>
2019-04-19 05:35:07 -0700

    commit

--- test.c
+++ test.c
@@ -1,11 +1,11 @@
 #include"test.h"
 #include<stdio.h>
 
-int add(int a, int b, int c)  %add 
+int add(int a, int b, int c)   
 {
 	int result = 0;
 	printf("%s", "Please enter the number:");
 	scanf("%d%d%d", &a, &b, &c);
-	result = a + b ;
+	result = a + b + c ;
 	return result;
 }
