166a16d37714f1549042c5eb4a02cbb7182719c3
Chenyang Zhang <695017814@qq.com>
2019-04-19 03:23:59 -0700

    test push

--- test.c
+++ test.c
@@ -6,6 +6,6 @@ int add(int a, int b, int c)  %add
 	int result = 0;
 	printf("%s", "Please enter the number:");
 	scanf("%d%d%d", &a, &b, &c);
-	result = a + b + c;
+	result = a + b ;
 	return result;
 }
